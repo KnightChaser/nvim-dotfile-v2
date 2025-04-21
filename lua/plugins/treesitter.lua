54247700af9cda07d45354545d115f6c6fa98bea
knightchaser <agerio100@naver.com>
2025-04-22 01:17:16 +0900

    Fix: Added nvim-treesitter dependency(nvim-treesitter/nvim-treesitter-textobjects) and keymap configurations

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,45 +1,50 @@
 -- lua/plugins/treesitter.lua
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate", -- keep parsers up‑to‑date
-  event = { "BufReadPre", "BufNewFile" }, -- lazy‑load on file open
-  opts = {
-    ensure_installed = { "c", "go", "python", "lua", "bash" },
-    auto_install = true, -- install missing parsers on enter
-    highlight = { enable = true },
-    indent = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "gnn",
-        node_incremental = "grn",
-        scope_incremental = "grc",
-        node_decremental = "grm",
-      },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
     },
-    textobjects = { -- optional: movement & selection
-      select = {
+    build = ":TSUpdate",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      ensure_installed = { "c", "go", "python", "lua", "bash" },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = {
         enable = true,
         keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
+          init_selection = "gnn",
+          node_incremental = "grn",
+          scope_incremental = "grc",
+          node_decremental = "grm",
         },
       },
-      swap = {
-        enable = true,
-        swap_next = { ["<leader>a"] = "@parameter.inner" },
-        swap_previous = { ["<leader>A"] = "@parameter.inner" },
-      },
-      move = {
-        enable = true,
-        goto_next_start = { ["]m"] = "@function.outer" },
-        goto_previous_start = { ["[m"] = "@function.outer" },
+      textobjects = {
+        select = {
+          enable = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = { ["<leader>a"] = "@parameter.inner" },
+          swap_previous = { ["<leader>A"] = "@parameter.inner" },
+        },
+        move = {
+          enable = true,
+          goto_next_start = { ["]m"] = "@function.outer" },
+          goto_previous_start = { ["[m"] = "@function.outer" },
+        },
       },
     },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
   },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-  end,
 }
