1e75e6612d16273dfd0d730393b026d6725069f5
knightchaser <agerio100@naver.com>
2025-04-22 00:11:32 +0900

    Chore: Refactor neo-tree.nvim configuration for further modularity

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,32 @@
+-- lua/plugins/neotree.lua
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  lazy = false,
+  opts = {
+    close_if_last_window = true,
+    filesystem = {
+      follow_current_file = { enabled = true },
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = true,
+      },
+    },
+    window = {
+      width = 30,
+      mappings = {
+        ["<cr>"] = "open",
+        ["S"] = "open_split",
+        ["<space>"] = "toggle_node",
+      },
+    },
+  },
+  keys = {
+    { "<leader>e", "<cmd>Neotree reveal<cr>", desc = "Toggle Neo‑tree" },
+  },
+}
