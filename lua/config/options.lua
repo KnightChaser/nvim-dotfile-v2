8e070aaaee9bcd51bf5888db5185b76de3266c2f
knightchaser <agerio100@naver.com>
2025-04-21 23:13:00 +0900

    Dev: Added basic core options about line numbers, mouse, clipboard, etc.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+
+-- line numbers, mouse, clipboard, etc.
+local o = vim.opt
+o.number = true
+o.relativenumber = true
+o.mouse = "a"
+o.clipboard = "unnamedplus"
+o.swapfile = false
+o.backup = false
+o.hlsearch = true
+o.incsearch = true
+o.termguicolors = true
+o.splitbelow = true
+o.splitright = true
+o.updatetime = 300
+o.timeoutlen = 500
