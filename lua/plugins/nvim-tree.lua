82eba46d2dc51a24252ffce79122ed8f044f4834
knightchaser <agerio100@naver.com>
2025-04-23 10:52:47 +0900

    Fix: Override <CR> key to focus the window before trying to open a specific file

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,47 +1,39 @@
 -- lua/plugins/nvim-tree.lua
 return {
   "nvim-tree/nvim-tree.lua",
-  requires = {
-    "nvim-tree/nvim-web-devicons", -- optional, for file icons
-  },
+  requires = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    -- Disable netrw at the very start of your init.lua
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-
-    -- Optionally enable 24-bit color
-    vim.opt.termguicolors = true
-
-    -- Set up nvim-tree with custom options
     require("nvim-tree").setup({
-      sort = {
-        sorter = "case_sensitive",
-      },
-      view = {
-        width = 30,
-      },
-      renderer = {
-        group_empty = true,
-      },
-      filters = {
-        dotfiles = true,
-      },
+      sort = { sorter = "case_sensitive" },
+      view = { width = 30 },
+      renderer = { group_empty = true },
+      filters = { dotfiles = true },
+
       on_attach = function(bufnr)
         local api = require("nvim-tree.api")
         local function opts(desc)
-          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+          return {
+            desc = "nvim-tree: " .. desc,
+            buffer = bufnr,
+            noremap = true,
+            silent = true,
+            nowait = true,
+          }
         end
 
-        -- Default mappings
+        -- load all the defaults
         api.config.mappings.default_on_attach(bufnr)
 
-        -- Custom mappings
+        -- OVERRIDE <CR> to open in the last-used window (no window picker)
+        vim.keymap.set("n", "<CR>", api.node.open.no_window_picker, opts("Open"))
+
+        -- other custom mappings
         vim.keymap.set("n", "<C-t>", api.tree.change_root_to_parent, opts("Up"))
         vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
       end,
     })
 
-    -- Global key mappings for `:NvimTreeToggle` and `:NvimTreeFindFile`
+    -- global toggles
     vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true, desc = "Toggle NvimTree" })
     vim.keymap.set(
       "n",
