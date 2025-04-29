f7316598193eb3a3990f3172690ebe2f5f115273
knightchaser <agerio100@naver.com>
2025-04-29 13:13:54 +0900

    Fix: Added a conditional logic at dropbar.lua to load the plugin only if the current neovim version is >=0.11.0 due to its requirements

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -1,6 +1,11 @@
 return {
   {
     "Bekaboo/dropbar.nvim",
+    -- only load on Neovim >= 0.11.0 due to its requirement.
+    cond = function()
+      -- returns 1 only if the version is 0.11.0 or higher
+      return vim.fn.has("nvim-0.11") == 1
+    end,
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
       build = "make",
@@ -8,13 +13,12 @@ return {
     config = function()
       require("dropbar").setup({
         bar = {
-          enable = function(buf, win, _)
+          enable = function()
             return true
           end,
         },
       })
 
-      -- keymaps
       local api = require("dropbar.api")
       vim.keymap.set("n", "<Leader>;", api.pick, { desc = "Pick symbols in winbar" })
       vim.keymap.set("n", "[;", api.goto_context_start, { desc = "Go to start of current context" })
