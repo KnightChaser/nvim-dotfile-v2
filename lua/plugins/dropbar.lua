d945e5842bf057ec81f2bae9a78cde03b150f544
knightchaser <agerio100@naver.com>
2025-04-30 09:44:41 +0900

    Fix: Delayed dropbar.lua plugin loading until the buffer comes in to avoid service crashes

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -1,28 +1,37 @@
 return {
   {
     "Bekaboo/dropbar.nvim",
-    -- only load on Neovim >= 0.11.0 due to its requirement.
     cond = function()
-      -- returns 1 only if the version is 0.11.0 or higher
+      -- only load on Neovim ≥ 0.11.0
       return vim.fn.has("nvim-0.11") == 1
     end,
+    -- load when opening or creating a file buffer
+    event = { "BufReadPost", "BufNewFile" },
     dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
     },
     config = function()
       require("dropbar").setup({
         bar = {
-          enable = function()
+          enable = function(bufnr, winid, ctx)
+            -- only in normal file buffers
+            local bt = vim.bo[bufnr].buftype
+            if bt ~= "" then
+              return false
+            end
             return true
           end,
         },
       })
 
       local api = require("dropbar.api")
-      vim.keymap.set("n", "<Leader>;", api.pick, { desc = "Pick symbols in winbar" })
-      vim.keymap.set("n", "[;", api.goto_context_start, { desc = "Go to start of current context" })
-      vim.keymap.set("n", "];", api.select_next_context, { desc = "Select next context" })
+      vim.keymap.set("n", "<Leader>;", api.pick,             { desc = "Pick symbols in winbar" })
+      vim.keymap.set("n", "[;",        api.goto_context_start, { desc = "Go to start of current context" })
+      vim.keymap.set("n", "];",        api.select_next_context, { desc = "Select next context" })
     end,
   },
 }
+
