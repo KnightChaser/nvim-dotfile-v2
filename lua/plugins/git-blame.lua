ff4d2acbabf4540f80ae2fb8868fc49ff425c96d
knightchaser <agerio100@naver.com>
2025-05-14 10:23:22 +0900

    Feat: Install git-blame.nvim plugin to Neovim

---
+++ lua/plugins/git-blame.lua
@@ -0,0 +1,11 @@
+-- lua/plugins/git-blame.lua
+return {
+  "f-person/git-blame.nvim",
+  event = "VeryLazy",
+  opts = {
+    enabled = true,
+    message_template = " <author> • <date> • <summary>",
+    date_format = "%r",
+    virtual_text_column = 1,
+  },
+}
