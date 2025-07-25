574f0e607675ea13b3b7489910c84a5cea78825d
knightchaser <agerio100@naver.com>
2025-07-25 20:50:44 +0900

    dev: modify barbar.nvim plugin keymaps from alt to space to avoid conflict with terminals

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,15 +17,16 @@ end, { silent = true, desc = "Jump to TS context" })
 -- barbar.nvim
 local map = vim.keymap.set
 local opts = { silent = true, noremap = true }
-map("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
-map("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
-map("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", opts)
-map("n", "<A->>", "<Cmd>BufferMoveNext<CR>", opts)
+map("n", "<leader>,", "<Cmd>BufferPrevious<CR>", opts)
+map("n", "<leader>.", "<Cmd>BufferNext<CR>", opts)
+map("n", "<leader><", "<Cmd>BufferMovePrevious<CR>", opts)
+map("n", "<leader>>", "<Cmd>BufferMoveNext<CR>", opts)
 for i = 1, 9 do
-  map("n", "<A-" .. i .. ">", "<Cmd>BufferGoto " .. i .. "<CR>", opts)
+  -- This will map <space>1, <space>2, etc. to switch buffers
+  map("n", "<leader>" .. i, "<Cmd>BufferGoto " .. i .. "<CR>", opts)
 end
-map("n", "<A-0>", "<Cmd>BufferLast<CR>", opts)
-map("n", "<A-p>", "<Cmd>BufferPin<CR>", opts)
+map("n", "<leader>0", "<Cmd>BufferLast<CR>", opts)
+map("n", "<leader>p", "<Cmd>BufferPin<CR>", opts)
 map("n", "<C-p>", "<Cmd>BufferPick<CR>", opts)
 map("n", "<leader>xbc", "<Cmd>BufferClose<CR>", opts)
 map("n", "<leader>xbac", "<Cmd>BufferCloseAllButCurrent<CR>", opts)
