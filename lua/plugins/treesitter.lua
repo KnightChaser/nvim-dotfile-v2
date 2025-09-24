1b55fa1b98236da563e20f4e82e4b3dd1ae26934
KnightChaser <agerio100@naver.com>
2025-09-24 13:31:04 +0900

    Chore: Add treesitter-family plugin to Neovim

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,8 +2,7 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    main = "nvim-treesitter.configs", -- <- let Lazy require this module
-    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+    main = "nvim-treesitter.configs",
     build = ":TSUpdate",
     event = { "BufReadPre", "BufNewFile" },
     opts = {
@@ -11,36 +10,68 @@ return {
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
+
+      -- Incremental selection (all under <leader>v …)
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "gnn",
-          node_incremental = "grn",
-          scope_incremental = "grc",
-          node_decremental = "grm",
+          -- TODO: Fix these keymaps to use <leader> later
+          init_selection = "m", -- start selection
+          node_incremental = "M", -- grow to next node
         },
       },
+
       textobjects = {
         select = {
           enable = true,
+          lookahead = true,
+          -- NOTE: these only apply in operator-pending/visual,
+          -- so 'af' won't hijack normal-mode 'a' anymore.
           keymaps = {
             ["af"] = "@function.outer",
             ["if"] = "@function.inner",
             ["ac"] = "@class.outer",
             ["ic"] = "@class.inner",
+            ["ap"] = "@parameter.outer",
+            ["ip"] = "@parameter.inner",
           },
-        },
-        swap = {
-          enable = true,
-          swap_next = { ["<leader>a"] = "@parameter.inner" },
-          swap_previous = { ["<leader>A"] = "@parameter.inner" },
         },
         move = {
           enable = true,
-          goto_next_start = { ["]m"] = "@function.outer" },
-          goto_previous_start = { ["[m"] = "@function.outer" },
+          set_jumps = true,
+          goto_next_start = { ["]m"] = "@function.outer", ["]]"] = "@class.outer" },
+          goto_next_end = { ["]M"] = "@function.outer", ["]["] = "@class.outer" },
+          goto_previous_start = { ["[m"] = "@function.outer", ["[["] = "@class.outer" },
+          goto_previous_end = { ["[M"] = "@function.outer", ["[]"] = "@class.outer" },
+        },
+        swap = {
+          enable = true,
+          swap_next = { ["<leader>sn"] = "@parameter.inner" },
+          swap_previous = { ["<leader>sp"] = "@parameter.inner" },
         },
       },
     },
+    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+  },
+  {
+    "danymat/neogen",
+    keys = {
+      {
+        "<leader>cn",
+        function()
+          require("neogen").generate()
+        end,
+        desc = "Generate doc",
+      },
+      {
+        "<leader>cN",
+        function()
+          require("neogen").generate({ type = "class" })
+        end,
+        desc = "Doc: class",
+      },
+    },
+    opts = { snippet_engine = "luasnip", enabled = true },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
   },
 }
