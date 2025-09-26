cfbca9d1fe90762e76f72db918d6b324f85e211d
KnightChaser <agerio100@naver.com>
2025-09-26 15:05:06 +0900

    Chore: Add a horizontal separator for treesitter-context.lua

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -7,13 +7,13 @@ return {
   },
   opts = {
     enable = true, -- turn the plugin on
+    line_numbers = true,
+    separator = "─",
     max_lines = 0, -- no limit
     min_window_height = 0, -- no limit
-    line_numbers = true,
     multiline_threshold = 20,
     trim_scope = "outer",
     mode = "cursor",
-    separator = nil,
     zindex = 20,
     on_attach = nil,
   },
